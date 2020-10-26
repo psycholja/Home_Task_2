abeea57623822e8d7cd4799c0ce67a7a762c48ee
psycholja <kolosowskaja@gmail.com>
2020-10-26 19:34:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Buffers;
+using System.IO;
+
+namespace Home_Task__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employees:\n");
+
+            using (StreamReader sr = File.OpenText("HT2.csv"))
+
+            {
+                string input = null;
+                while ((input = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(input);
+                }
+            }
+                    Console.ReadLine();
+                
+            }
+
+        }
+    }
+
+        
+        
+       
